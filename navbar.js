cf25f0dbf1955006682e58d5d086e4a3caba9817
Rohana Taher Chowdhury <sadafchowdhury458@gmail.com>
2026-06-20 09:06:29 +0600

    first commit

---
+++ navbar.js
@@ -0,0 +1,49 @@
+const tailwindScript = document.createElement("script");
+tailwindScript.src = "https://cdn.tailwindcss.com";
+document.head.appendChild(tailwindScript);
+
+const navbar = document.createElement("nav");
+
+navbar.innerHTML = `
+  <div class="fixed top-0 left-0 w-full bg-slate-50 backdrop-blur-md z-50 border-b border-gray-300">
+    <div class="max-w-6xl mx-auto px-6 py-4 flex items-center justify-between">
+
+      <!-- Logo -->
+      <h2 class="text-2xl font-bold text-emerald-600">
+        My Portfolio
+      </h2>
+
+      <!-- Desktop Menu -->
+      <ul class="hidden md:flex items-center gap-8 text-gray-700 font-medium">
+        <li><a href="hero section.html" class="hover:text-emerald-700 transition">Home</a></li>
+        <li><a href="about.html" class="hover:text-emerald-700 transition">About</a></li>
+        <li><a href="projects.html" class="hover:text-emerald-700 transition">Projects</a></li>
+        <li><a href="contact.html" class="hover:text-emerald-700 transition">Contact</a></li>
+      </ul>
+
+      <!-- Mobile Button -->
+      <button id="menuBtn" class="md:hidden text-black text-3xl">
+        ☰
+      </button>
+    </div>
+
+    <!-- Mobile Menu -->
+    <div id="mobileMenu" class="hidden md:hidden bg-gray-900 px-6 pb-4">
+      <a href="hero section.html" class="block py-2 text-gray-300 hover:text-cyan-400">Home</a>
+      <a href="about.html" class="block py-2 text-gray-300 hover:text-cyan-400">About</a>
+      <a href="projects.html" class="block py-2 text-gray-300 hover:text-cyan-400">Projects</a>
+      <a href="contact.html" class="block py-2 text-gray-300 hover:text-cyan-400">Contact</a>
+    </div>
+  </div>
+`;
+
+document.body.prepend(navbar);
+
+setTimeout(() => {
+  const menuBtn = document.getElementById("menuBtn");
+  const mobileMenu = document.getElementById("mobileMenu");
+
+  menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+  });
+}, 100);
